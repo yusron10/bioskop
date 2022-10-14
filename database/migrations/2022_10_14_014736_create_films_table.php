aad9ef198ab22aefd2977bad193d7422855ce143
yusron10 <my973087@gmail.com>
2022-10-14 08:58:35 +0700

    two commit

---
+++ database/migrations/2022_10_14_014736_create_films_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('tahun');
+            $table->string('deskripsi');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+};
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.template')
 
+@section ('title', 'Home')
+
 @section('background')
     
 
@@ -9,7 +11,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
 </head>
 <body>
     <h1>Halaman Home</h1>
